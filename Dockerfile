307cf7922ae78ae25716acebd7e87944db5acc50
aformusatii <aformusatii@gmail.com>
2023-11-03 23:25:55 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM alpine
+#
+RUN set -xe \
+    && apk add --no-cache --purge -uU apache2-utils squid curl \
+    #&& ln -sf python3 /usr/bin/python \
+    #&& python3 -m ensurepip \
+    #&& pip3 install --no-cache --upgrade pip setuptools \
+    && rm -rf /var/cache/apk/* /tmp/* 
+#
+COPY squid/squid.with.auth.conf /etc/squid/squid.with.auth.conf
+COPY squid/squid.without.auth.conf /etc/squid/squid.without.auth.conf
+
+ADD squid/run.sh /root/run.sh
+
+EXPOSE 3128
+#
+#ENTRYPOINT ["squid", "-NYCd", "5", "-f", "/etc/squid/squid.conf"]
+ENTRYPOINT ["sh", "/root/run.sh"]
---
+++ squid/run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ -z "${SQUID_USERNAME}" ] || [ -z "${SQUID_PASSWORD}" ]
+then
+    echo "SQUID_USERNAME and SQUID_PASSWORD variables are not set, skip."
+    
+    echo "Use squid.without.auth.conf!"
+    cp -f /etc/squid/squid.without.auth.conf /etc/squid/squid.conf
+else
+    echo "Generate /etc/squid/passwords!"
+    htpasswd -b -c /etc/squid/passwords "${SQUID_USERNAME}" "${SQUID_PASSWORD}"
+    
+    echo "Use squid.with.auth.conf!"
+    cp -f /etc/squid/squid.with.auth.conf /etc/squid/squid.conf
+fi
+
+if [ -z "${SHOW_ACCESS_LOGS}" ]
+then
+    echo "SHOW_ACCESS_LOGS variable is not set, skip."
+else
+    tail -F /var/log/squid/access.log &
+fi
+
+if [ -z "${CLOUDFLARE_API_KEY}" ] || [ -z "${CLOUDFLARE_DNS}" ]
+then
+    echo "CLOUDFLARE_API_KEY and CLOUDFLARE_DNS variables are not set, skip."
+else
+    curl -k -d "{\"apiKey\":\"${CLOUDFLARE_API_KEY}\", \"host\":\"${CLOUDFLARE_DNS}\"}" -H "Content-Type: application/json" -X POST https://new.htmlsketcher.com/update-ip
+fi
+
+squid -NYCd 5 -f /etc/squid/squid.conf
