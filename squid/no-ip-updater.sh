d683f1042c92ac9ab53a2e1c21593ef7c8b4d581
Andrei <aformusatii@gmail.com>
2023-11-06 00:15:31 +0200

    Updates

--- squid/no-ip-updater.sh
+++ squid/no-ip-updater.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Check if the necessary environment variables are set
-if [ -n "$USERNAME" ] && [ -n "$PASSWORD" ] && [ -n "$HOSTNAME" ]; then
+if [ -n "$NO_IP_USERNAME" ] && [ -n "$NO_IP_PASSWORD" ] && [ -n "$NO_IP_HOSTNAME" ]; then
+    USERNAME="$NO_IP_USERNAME"
+    PASSWORD="$NO_IP_PASSWORD"
+    HOSTNAME="$NO_IP_HOSTNAME"
+
     # No-IP Update URL
     UPDATE_URL="https://dynupdate.no-ip.com/nic/update"
 
@@ -14,9 +18,9 @@ if [ -n "$USERNAME" ] && [ -n "$PASSWORD" ] && [ -n "$HOSTNAME" ]; then
             IP=$(curl -s https://ipv4.icanhazip.com)
             if [ -n "$IP" ]; then
                 RESULT=$(curl -s -u "$USERNAME:$PASSWORD" -X GET "$UPDATE_URL?hostname=$HOSTNAME&myip=$IP")
-                echo "$(date +'%Y-%m-%d %H:%M:%S') - $RESULT" >> "$LOG_FILE"
+                echo "$(date +'%Y-%m-%d %H:%M:%S') - $RESULT"
             else
-                echo "$(date +'%Y-%m-%d %H:%M:%S') - Failed to fetch local IP address" >> "$LOG_FILE"
+                echo "$(date +'%Y-%m-%d %H:%M:%S') - Failed to fetch local IP address"
             fi
             sleep 3600  # Sleep for 1 hour (adjust as needed)
         done
@@ -25,5 +29,5 @@ if [ -n "$USERNAME" ] && [ -n "$PASSWORD" ] && [ -n "$HOSTNAME" ]; then
     # Call the update function in the background
     update_noip &
 else
-    echo "Warning: USERNAME, PASSWORD, and HOSTNAME environment variables are not set. Skipping NO_IP update."
+    echo "Warning: NO_IP_USERNAME, NO_IP_PASSWORD, and NO_IP_HOSTNAME environment variables are not set. Skipping update."
 fi
